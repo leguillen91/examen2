65fb956c7debe9240f781b110f6e856cafef699c
leguillen91 <luisguillenortiz_8@hotmail.com>
2024-04-05 17:54:50 -0500

    Proyecto Base

---
+++ src/test/java/hn/unah/examen/Examen2ApplicationTests.java
@@ -0,0 +1,13 @@
+package hn.unah.examen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examen2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
